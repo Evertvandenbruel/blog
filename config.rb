4b2fe98bc817bd4c4927903824436388c3b8bce4
evert <evert@10to1.be>
2013-01-13 16:50:41 +0100

    initial commit

---
+++ config.rb
@@ -0,0 +1,16 @@
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/Evertvandenbruel/"
+http_images_path = "/Evertvandenbruel/images"
+http_fonts_path = "/Evertvandenbruel/fonts"
+css_dir = "public/Evertvandenbruel/stylesheets"
+
+# Local development paths
+sass_dir = "sass"
+images_dir = "source/images"
+fonts_dir = "source/fonts"
+
+line_comments = false
+output_style = :compressed
